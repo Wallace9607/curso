7aae18bc256cbdc579107e93313f870748ee3fdf
Wallace9607 <156460150+Wallace9607@users.noreply.github.com>
2025-11-19 18:11:21 -0300

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,13 @@
+const modules = [
+  { title:"Introdução à Engenharia de Software", image:"img/gfds.jpg", link:"introducao_engenharia.html" },
+  { title:"Lógica de Programação", image:"img/logica-programacao.jpeg", link:"logica_programacao.html" },
+  { title:"Matemática Discreta", image:"img/matematica-discreta.jpeg", link:"matematica_discreta.html" },
+  { title:"Fundamentos de Computação", image:"img/fundamentos-computacao.jpeg", link:"fundamentos_computacao.html" },
+  { title:"Estrutura de Dados", image:"img/estrutura-dados.jpeg", link:"estrutura_dados.html" },
+  { title:"POO", image:"img/poo.jpg", link:"poo.html" },
+  { title:"Banco de Dados I", image:"img/banco-dados1.jpeg", link:"banco_dados1.html" },
+  { title:"Sistemas Operacionais", image:"img/sistemas-operacionais.jpeg", link:"sistemas_operacionais.html" },
+  { title:"Engenharia de Requisitos", image:"img/engenharia-requisitos.jpeg", link:"engenharia_requisitos.html" },
+  { title:"Redes de Computadores", image:"img/redes.jpeg", link:"redes.html" }
+  // (adicione o resto da matriz)
+];
